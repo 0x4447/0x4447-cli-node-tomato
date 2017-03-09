297d5871724c6c2baf20f4c39106c6d211460652
David Gatti <office@gatti.pl>
2017-03-09 08:44:11 +0100

    Added CLI support

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
+#!/usr/bin/env node
+
 let fs = require('fs');
 let path = require('path');
 
-let source = "source"
-let destination = "destination"
+let source = "source";
 
-copy_folder(source, destination);
+//
+//
+//
+let destination_path = process.argv[1].split('/');
+
+//
+//    1. Extract the name of the app/file name.
+//
+let appName = destination_path.pop();
+
+//
+//    2. Save the first provided argument as the token.
+//
+const destination = process.argv[2];
+
+//
+//    4. Check if the secret was provided.
+//
+if(!destination)
+{
+    //
+    //    1. Give the user an example how to use the app.
+    //
+    console.log("Missing argument! \n\n\tExample: ./%s \"THE_PAYLOAD\" YOUR_TOKEN\n", appName);
+
+    //
+    //    -> Exit the app if error.
+    //
+    process.exit(1);
+}
+
+//
+//
+//
+destination_path.push(destination)
+
+//
+//
+//
+let destination_path_string = destination_path.join("/")
+
+
+//
+//
+//
+copy_folder(source, destination_path_string);
 
 //
 //
