13a25c042a8f8c7bd84690b4512e83cdc16168a9
David Gatti <office@gatti.pl>
2017-03-18 13:10:20 +0100

    Typo

--- source/routes/index.js
+++ source/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 	res.render("_frame", {
 		title: "Home",
 		description: "Home Page",
-		og_image: "images/og.jpg"
+		og_image: "images/og.jpg",
 		partials: {
 			body: 'index'
 		}
